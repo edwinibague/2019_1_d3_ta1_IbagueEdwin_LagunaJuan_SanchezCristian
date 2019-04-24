9c1778ecaf334b589af1c6a52ad44fd3b0debb80
giovannysanchezusta <cristiansanchezv@usantotomas.edu.co>
2019-04-24 01:06:24 -0500

    algo anda mal

--- filtro.cpp
+++ filtro.cpp
@@ -12,11 +12,10 @@
 #include <sys/stat.h>
 #include <cmath>
 #include "image.h"
-//#include <Image.cpp>
 #include <omp.h>
 
 using namespace std;
-//home/giovanny/Digitales/tarea_digitales/2019_1_d3_ta1_IbagueEdwin_LagunaJuan_SanchezCristian/
+///home/giovanny/Digitales/tarea_digitales/2019_1_d3_ta1_IbagueEdwin_LagunaJuan_SanchezCristian/
 int main (){
                                                                                                                        
     string name;
@@ -43,18 +42,18 @@ int main (){
      int newpixel;
      double Gx [5][5] = {
      				 {0 , 0   , 0 , 0   , 0},
-                     {0 , 0.5 , 0 ,-0.5 , 0},
+                     {0 , 0.5 , 0 , -0.5, 0},
                      {0 , 1   , 0 , -1  , 0},
                      {0 , 0.5 , 0 , -0.5, 0},
                      {0 , 0   , 0 , 0   , 0}
                  };
 
      double Gy [5][5] = {
-     				 {0 , 0   , 0 , 0   ,0},
-                     {0 , 0.5 , 1 , 0.5 ,0},
-                     {0 , 0   , 0 , 0   ,0},
-                     {0 ,-0.5 , -1, -0.5,0},
-                     {0 , 0   , 0 , 0   ,0}
+     				 {0 , 0   , 0   , 0    ,0},
+                     {0 , 0.5 , 1   , 0.5  ,0},
+                     {0 , 0.5 , 0.5 , 0.5  ,0},
+                     {0 ,-0.5 , -1  , -0.5 ,0},
+                     {0 , 0   , 0   , 0    ,0}
                  };
 
      tamano=sqrt(sizeof(Gx)/sizeof(Gx[0][0]));
@@ -63,17 +62,7 @@ int main (){
      cout<<a<<endl;
      int b= Imagen_in.height;
      cout<<b<<endl;
-    // cout << sizeof(myImage.data) << endl;
-    // printf("%d\n",(int)myImage.data[4143*a+3912]);
-    // //printf("%d\n",myImage[4143][3912]);
-    // cout << "aaa" << endl;
-
-		//omp_set_num_threads(4);
-    	//int height = 0;
-        //#pragma omp parallel for reduction(+:height)
-
-        //{
-
+   
          for(int i=0;i<Imagen_in.height;i++){
 
             for(int j=0;j<Imagen_in.width;j++){
@@ -81,7 +70,10 @@ int main (){
                  x=i-1;
                  y=j-1;
 
-                 #pragma omp parallel for num_threads(threads) 
+                 int k;
+                 int l;
+
+                 #pragma omp parallel for reduction (+:threads)//num_threads(threads)  
 
                  for(int k=0;k<tamano;k++){                    
                         
@@ -99,22 +91,18 @@ int main (){
 
             newpixel=sqrt((acum_convx*acum_convx)+(acum_convy*acum_convy));// el nuevo pixel
             if(newpixel<0){
-                 newpixel=0;
+                newpixel=0;
             }
             if(newpixel>255){
-                 newpixel=255;
+                newpixel=255;
             }
+
             filt[i*Imagen_in.width+j]=newpixel;
             acum_convx=0;
             acum_convy=0;
             }
 
         }
-
-        //}// Fin pragma
-
-        //height *= 0; 
-
 
      Imagen_filtrada.data=filt;
      bandera=Imagen_filtrada.toFile(out_path);
